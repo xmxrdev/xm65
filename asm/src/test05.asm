fa2cad5d831aa79bb9c13ac28b6594844067785e
XMXR <xmxrhxr@gmail.com>
2026-01-21 19:36:40 -0600

    Addressing modes absolute,x and indirect,x were tested.

--- asm/src/test05.asm
+++ asm/src/test05.asm
@@ -2,9 +2,45 @@
 
 .org $0200
 
-; Testing addressing modes
-lda #$ff
-sta $fa00
+;;; Testing addressing modes
+
+; Draw 4 pixels spaced by 1 pixel on the top-left corner of the screen
+    lda #$ff ; White
+    ldx #$01
+    sta $fb00,x
+    ldx #$02
+    sta $fb00,x
+    ldx #$03
+    sta $fb00,x
+    ldx #$04
+    sta $fb00,x
+
+; Testing indirect mode
+    COLOR_RED   := %11100000
+    COLOR_GREEN := %00011100
+    COLOR_BLUE  := %00000011
+
+    lda #$20
+    sta $00
+    lda #$fb
+    sta $01
+
+    ldx #$00
+
+    lda #COLOR_RED
+    sta ($00,x) ; Store color in $fb20 indirectly
+
+    lda #$21
+    sta $00
+
+    lda #COLOR_GREEN
+    sta ($00,x) ; Store color in $fb21 indirectly
+
+    lda #$fc
+    sta $01
+
+    lda #COLOR_BLUE
+    sta ($00,x) ; Store color in $fc21 indirectly
 
 brk
 
@@ -15,7 +51,7 @@ RESET:
     ldx #$ff
     txs
     cli
-    jmp $0300
+    jmp $0200
 
 NMI:
     rti
